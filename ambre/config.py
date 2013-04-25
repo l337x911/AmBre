d6d62bbb6e38661ef0b8813f96c5e933f17ef912
Anand D. Patel <patel.anandd@gmail.com>
2013-04-25 16:54:14 -0700

    Small bug in config.py

--- ambre/config.py
+++ ambre/config.py
@@ -16,42 +16,47 @@
 #  For any other inquiries send an email to: Anand D. Patel (adp002@ucsd.edu)
 '''
 
-from pkg_resources import resource_filename, resource_stream
-import os
+from pkg_resources import resource_filename
+import os, sys
 
 class Config(object):  
   def __init__(self):
-    self.config_name = 'ambre.conf'
+    self.config_fpath = resource_filename(__name__, 'ambre.conf')
     self.bin = dict()
     self.dir = dict()
     self.param = dict()
 
     # Defaults
-    self.param['primer3_short'] = resource_filename(__name__, os.path.join('data','primer3_v1_1_4_default_settings.txt'))
+    #self.param['primer3_short'] = resource_filename(__name__, os.path.join('data','primer3_v1_1_4_default_settings.txt'))
     self.param['primer3_long'] = resource_filename(__name__, os.path.join('data','primer3_lr_A04.txt'))
-
+    self.dir['examples'] = None
   def add_bin(self, bin_name, bin_path):
     self.bin[bin_name]= bin_path
   def add_param(self, param_name, param_path):
     self.param[param_name] = param_path
   def load(self):
-    for line in resource_stream(__name__, self.config_name):
-      if line.startswith('#'): continue
-      tokens = line.strip().split('=')
-      try:
-        key_name, key_path = tokens
-      except:
-        continue
-      if key_name.endswith('_bin'):
-        self.bin[key_name[:-4]] = key_path
-      elif key_name.endswith('_p'):
-        self.param[key_name[:-2]] = key_path
-      elif key_name.endswith('_dir'):
-        self.dir[key_name[:-4]] = key_path
-  def valid(self):
-    pass
-  def replace(self):
-    pass
+    with open(self.config_fpath) as f:
+      for line in f:
+        if line.startswith('#'): continue
+        tokens = line.strip().split('=')
+        try:
+          key_name, key_path = tokens
+        except:
+          continue
+        if key_name.endswith('_bin'):
+          self.bin[key_name[:-4]] = key_path
+        elif key_name.endswith('_p'):
+          self.param[key_name[:-2]] = key_path
+        elif key_name.endswith('_dir'):
+          self.dir[key_name[:-4]] = key_path
+  def update(self, config_fpath):
+    try:
+      assert os.path.isfile(config_fpath)
+      self.config_fpath = config_fpath
+      self.load()
+    except AssertionError:
+      print "Error: Config path %s does not exist."%config_fpath
+      sys.exit(1)
 
 CONFIG = Config()
 CONFIG.load()
